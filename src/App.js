4c0b84771c607ee9065034ce033a93ffbb1da981
kunal-ojha <kunal.ojha1@t-mobile.com>
2022-04-08 17:04:00 -0500

    [Section-3] Renders and Re-renders in React

--- src/App.js
+++ src/App.js
@@ -7,32 +7,38 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: 'Kunal', lastName: 'Ojha' },
-      company: 'NBC'
-    }
+      monsters: []
+    };
+
+    console.log('constructor runs...')
   }
 
+  // Runs when component mounts. Mounting is when the component first gets placed on the DOM. Only happens once. 
+  // Typically API calls should go here.
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users =>
+        this.setState(() => {
+          return { monsters: users }
+        }));
+
+        console.log('componentDidMount runs...')
+  }
 
   render() {
+    console.log('render runs...')
     return (
       <div className="App">
-        {<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
-          <button onClick={() => {
-            this.setState(() => {
-              return {
-                name: { firstName: 'Vichie' }, company: 'Qatar Airways'
-              }
-            },
-              () => {
-                console.log('state is ready', this.state)
-              });
-            console.log(this.state);
-          }}>Change Name</button>
-        </header>}
+        {this.state.monsters.map(monster => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })}
       </div>
-    )
+    );
   }
 }
 
