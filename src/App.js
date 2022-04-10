621f71d20baad29b52436ac43b0cae3b7a33d779
Kunal Ojha <kunalojha12@gmail.com>
2022-04-09 21:04:48 -0500

    [Section-3] Storing original data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  monsters = [];
+
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
-  }
+  };
 
   // Runs when component mounts. Mounting is when the component first gets placed on the DOM. Only happens once. 
   // Typically API calls should go here.
@@ -18,14 +20,27 @@ class App extends Component {
       .then(response => response.json())
       .then(users =>
         this.setState(() => {
+          this.monsters = users;
           return { monsters: users }
         }))
   }
 
   render() {
+    console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map(monster => {
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          const searchField = event.target.value.toLocaleLowerCase();
+          this.setState(() => {
+            return { searchField }
+          })
+        }} />
+        {filteredMonsters.map(monster => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
